1d53fbddfb66f29abeb01b122353d4b63fcaed43
cyrus <cyruskeo99@gmail.com>
2018-11-29 17:19:25 -0700

    Finished burger functionality, not happy with the CSS

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name, devoured) VALUES ("big burger", true); 
+
+INSERT INTO burgers (name, devoured) VALUES ("little burger", true);
+
+INSERT INTO burgers (name, devoured) VALUES ("bad burger", false);
